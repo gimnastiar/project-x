067cb2f72573f6ab2e6adee75a6d95b8088e33f5
gimnastiar <46832633+gimnastiar@users.noreply.github.com>
2019-04-07 20:18:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// buat onload
+
+$(window).on('load', function(){
+    $('.text1').addClass('ada');
+    $('.text2').addClass('ada');
+    $('.img1').addClass('ada');
+    $('.newt').addClass('ada');
+   
+
+
+
+});
